b7a937725b2e8c3a84cc942d5c90888b7f2b134c
Pablo Feldman <pablo.feldman@globant.com>
2016-01-22 12:12:06 -0300

    Tidy up code and set PhantomJS as browser

--- tests/person.spec.js
+++ tests/person.spec.js
@@ -1,13 +1,3 @@
-/*require('[src/person.js]', function(person){
-    describe('Person ', function(){
-        it('Should say hi with proposed first name and last name', function() {
-            var person = new person();
-            expect(true).toEqual(true);
-        });
-    });
-});
-*/
-
 require(["/base/src/person.js"], function () {
     describe('Person ', function(){
         it('Should say hi with proposed first name and last name', function() {
@@ -17,14 +7,3 @@ require(["/base/src/person.js"], function () {
         });
     });
 });
-
-
-/*define(['person'], function(Person) {
-    describe('Person ', function(){
-        it('Should say hi with proposed first name and last name', function() {
-            //var person = new Person();
-            expect(true).toEqual(true);
-        });
-    });
-});
-*/
