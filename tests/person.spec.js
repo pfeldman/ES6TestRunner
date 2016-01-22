d47fea40f49e600d2e88ddfe5d84ff8b388560a3
Pablo Feldman <pablo.feldman@globant.com>
2016-01-22 10:34:53 -0300

    Make libraries locally and fix tests

--- tests/person.spec.js
+++ tests/person.spec.js
@@ -11,9 +11,9 @@
 require(["/base/src/person.js"], function () {
     describe('Person ', function(){
         it('Should say hi with proposed first name and last name', function() {
-            var person = new Person("Pablo", "Feldman");
-            var salut = person.sayHi();
-            expect(salut).toEqual("Hi, I'm Pablo Feldman");
+            let person = new Person("John", "Doe");
+            let salut = person.sayHi();
+            expect(salut).toEqual("Hi, I'm John Doe");
         });
     });
 });
